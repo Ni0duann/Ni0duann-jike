0eea1c89f626e9c4d683e293736df0a22c2c13b9
Ni0duann <2014567981@qq.com>
2024-09-27 21:47:23 +0800

    登陆-axios封装处理

---
+++ src/utils/request.js
@@ -0,0 +1,28 @@
+//axios 封装处理
+
+import axios from "axios";
+// 1.根域名配置
+// 2.超时时间
+// 3.请求拦截器 /响应拦截器
+
+const request = axios.create({
+    baseURL:'http://geek.itheima.net/v1_0',
+    timeout: 5000
+})
+
+// 请求拦截器
+request.interceptors.request.use((config) =>{
+    return config
+},(error)=>{
+    return Promise.reject(error)
+})
+
+// 相应拦截器
+request.interceptors.request.use((config) =>{
+    return Response.data
+},(error)=>{
+    // 超出2xx会触发
+    return Promise.reject(error)
+})
+
+export  {request}
