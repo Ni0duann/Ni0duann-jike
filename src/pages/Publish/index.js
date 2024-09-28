2247ae052963f9ce7a017f70fa8b9b6d56ef53c3
Ni0duann <2014567981@qq.com>
2024-09-28 13:59:39 +0800

    二级路由

---
+++ src/pages/Publish/index.js
@@ -0,0 +1,5 @@
+const Pubish = () => {
+    return <div>this is Pubish</div>
+}
+
+export default Pubish
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,30 @@
 //路由配置
 import { AuthRoute } from "@/components/AuthRoute";
+import Article from "@/pages/Article";
+import Home from "@/pages/Home";
 import Layout from "@/pages/Layout";
 import Login from  "@/pages/Login"
+import Pubish from "@/pages/Publish";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter ([
     {
         path: "/",
-        element: <AuthRoute> <Layout /></AuthRoute>
+        element: <AuthRoute> <Layout /></AuthRoute>,
+        children: [
+            {
+                index:'true',
+                element: <Home />
+            },
+            {
+                path:'article',
+                element:<Article />
+            },
+            {
+                path:'publish',
+                element:<Pubish />
+            }
+        ]
     },
     {
         path:"/login",
