1157fe692f6b8e08d48b77b1325606e77bb33093
Ni0duann <2014567981@qq.com>
2024-09-28 12:35:57 +0800

    根据token控制路由权限

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 //路由配置
+import { AuthRoute } from "@/components/AuthRoute";
 import Layout from "@/pages/Layout";
 import Login from  "@/pages/Login"
 import { createBrowserRouter } from "react-router-dom";
@@ -6,7 +7,7 @@ import { createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter ([
     {
         path: "/",
-        element: <Layout />
+        element: <AuthRoute> <Layout /></AuthRoute>
     },
     {
         path:"/login",
