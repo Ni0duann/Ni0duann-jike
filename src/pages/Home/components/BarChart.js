e60e225a692f749cce800ed902f915e6c2cb50ea
Ni0duann <2014567981@qq.com>
2024-09-29 11:43:14 +0800

    echarts-组件封装

---
+++ src/pages/Home/components/BarChart.js
@@ -0,0 +1,41 @@
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+
+
+const BarChart  = ({title}) =>{
+    const chartRef = useRef(null);
+    useEffect(() => {
+        // 保证dom可用才进行图表的渲染
+        //获取渲染图标的dom节点
+        const chartDom = chartRef.current;
+        //图标初始化生成
+        const myChart = echarts.init(chartDom);
+
+        //图表参
+        const option = {
+        title:{
+            text: title
+        },
+        xAxis: {
+            type: "category",
+            data: ["Vue", "React", "Angular"],
+        },
+        yAxis: {
+            type: "value",
+        },
+        series: [
+            {
+            data: [10, 40, 70],
+            type: "bar",
+            },
+        ],
+        };
+        //使用图标参数完成渲染
+        option && myChart.setOption(option);
+    }, []);
+    return (
+          <div ref={chartRef} style={{ width: "500px", height: "400px" }}></div>
+      );
+}
+
+export default BarChart
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,40 +1,12 @@
-import * as echarts from "echarts";
-import { useEffect, useRef } from "react";
-
+import BarChart from "./components/BarChart";
 
 const Home = () => {
-    const chartRef = useRef(null)
-    useEffect(() => {
-        // 保证dom可用才进行图表的渲染
-        //获取渲染图标的dom节点
-        const chartDom = chartRef.current
-        //图标初始化生成
-        const myChart = echarts.init(chartDom);
-
-        //图表参
-        const option = {
-            xAxis: {
-            type: "category",
-            data: ["Vue","React","Angular"],
-            },
-            yAxis: {
-                type: "value",
-            },
-            series: [
-                {
-                data: [10, 40, 70],
-                type: "bar",
-                },
-            ],
-        };
-    //使用图标参数完成渲染
-    option && myChart.setOption(option);
-    }, []);
-    return (
-        <div>
-        <div ref={chartRef} style={{ width: "500px", height: "400px" }}></div>
-        </div>
-    );
+  return (
+    <div>
+      <BarChart title={'三大框架满意度'} />
+      <BarChart title={'三大框架使用度'} />
+    </div>
+  )
 };
 
 export default Home;
