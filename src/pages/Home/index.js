ad2e44e7e4749de0ff0138dbd6dcbfe7953a2863
Ni0duann <2014567981@qq.com>
2024-09-29 11:17:27 +0800

    echarts-图表初始化

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,40 @@
-const Home = () => {
-    return <div>this is Home</div>
-}
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
 
-export default Home
+
+const Home = () => {
+    const chartRef = useRef(null)
+    useEffect(() => {
+        // 保证dom可用才进行图表的渲染
+        //获取渲染图标的dom节点
+        const chartDom = chartRef.current
+        //图标初始化生成
+        const myChart = echarts.init(chartDom);
+
+        //图表参
+        const option = {
+            xAxis: {
+            type: "category",
+            data: ["Vue","React","Angular"],
+            },
+            yAxis: {
+                type: "value",
+            },
+            series: [
+                {
+                data: [10, 40, 70],
+                type: "bar",
+                },
+            ],
+        };
+    //使用图标参数完成渲染
+    option && myChart.setOption(option);
+    }, []);
+    return (
+        <div>
+        <div ref={chartRef} style={{ width: "500px", height: "400px" }}></div>
+        </div>
+    );
+};
+
+export default Home;
