9bbbe11ea80e2cbd2a66d9251e158c010405c4cf
Ni0duann <2014567981@qq.com>
2024-10-07 19:10:45 +0800

    编辑功能-完善数据回填

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -64,7 +64,16 @@ const Publish = () => {
   useEffect (() => {
     async function getArticleDetail () {
       const res = await getArticlelById(articleId)
-      form.setFieldsValue(res.data)
+      const data = res.data
+      const {cover} = data
+      form.setFieldsValue({
+        ...data,
+        type: cover.type
+      })
+      setImageType(cover.type)
+      setImageList(cover.images.map(url=>{
+        return {url}
+      }))
     }
     getArticleDetail()
   },[articleId,form])
@@ -111,6 +120,7 @@ const Publish = () => {
                 name="image"
                 onChange={onChange}
                 maxCount={imageType}
+                fileList={imageList}
               >
                 <div style={{ marginTop: 8 }}>
                   <PlusOutlined />
